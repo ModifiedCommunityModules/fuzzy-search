35c55cb9e64f7abe11602277ea1677d61e9f2965
php-schubser <noreply@fishnet-services.com>
2021-09-16 13:53:28 +0200

    chore: update display of keywords / products

--- new_files/includes/extra/modules/product_listing_split/mcm_fuzzy_search.php
+++ new_files/includes/extra/modules/product_listing_split/mcm_fuzzy_search.php
@@ -1,6 +1,5 @@
 <?php
 use ModifiedCommunityModules\FuzzySearch\Classes\FuzzySearch;
-
 // wenn keywords aus Suche übergeben -> fuzzy search
 if (!isset($_GET['keywords']) || $_GET['keywords'] == '' || MODULE_MCM_FUZZY_SEARCH_ACTIVATE_SUGGEST != 'true' || MODULE_MCM_FUZZY_SEARCH_STATUS != 'true') return;
 
@@ -16,18 +15,23 @@ $module_content_keywords = $Suggest->resultKeywords;
 $module_content_products = $Suggest->resultProducts;
 $parse_time = $Suggest->parse_time;
 
+$info_smarty= new Smarty;
+$info_smarty->assign('tpl_path','templates/' . CURRENT_TEMPLATE . '/');
+$info_smarty->assign('language', $_SESSION['language']);
+
 if ($module_content_keywords) {
-  $smarty->assign('keyword_data', $module_content_keywords);
+  $info_smarty->assign('keyword_data', $module_content_keywords);
 }
 if ($module_content_products) {
-  $info_smarty= new Smarty;
-  $info_smarty->assign('tpl_path','templates/' . CURRENT_TEMPLATE . '/');
-  $info_smarty->assign('language', $_SESSION['language']);
   $info_smarty->assign('module_content', $module_content_products);
-  $info_smarty->caching = 0;
-  $smarty->assign('suggest_products', $info_smarty->fetch(CURRENT_TEMPLATE . '/module/mcm_fuzzy_search.html'));
 }
 
 if (SEARCH_SHOW_PARSETIME == 'true') {
-  $smarty->assign('PARSE_TIME', '<small>' . $parse_time . ' s</small>');
+  $info_smarty->assign('PARSE_TIME', '<small>' . $parse_time . ' s</small>');
+}
+
+$info_smarty->caching = 0;
+
+if ($module_content_keywords || $module_content_products) {
+  $smarty->assign('mcm_fuzzy_search', $info_smarty->fetch(CURRENT_TEMPLATE . '/module/mcm_fuzzy_search.html'));
 }
